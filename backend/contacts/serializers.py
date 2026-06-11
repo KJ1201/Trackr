46afdaa1b6df8202d34a4c3dccef327b7eb72229
KJ1201 <karanjuneja2006@outlook.com>
2026-06-11 07:15:23 +0530

    Job Tracker v1 complete

--- backend/contacts/serializers.py
+++ backend/contacts/serializers.py
@@ -1,8 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Contact
 
 class ContactSerializer(ModelSerializer):
+    application_name = SerializerMethodField()
     class Meta:
         model = Contact
-        fields = ['id', 'name', 'role', 'user', 'created_at', 'email', 'linkedin_url', 'note', 'application']
+        fields = ['id', 'name', 'role', 'user', 'created_at', 'email', 'linkedin_url', 'note', 'application', 'application_name']
         read_only_fields = ('id', 'user', 'created_at')
+
+    def get_application_name(self, obj):
+        if obj.application:
+            return f"{obj.application.name} at {obj.application.company}"
+        return None
